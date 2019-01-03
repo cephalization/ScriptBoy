d22473f3a654d95d445021ec49e3b828470dbc7b
Anthony Powell <anthony.powell@ambyint.com>
2019-01-02 22:47:20 -0500

    Refactor player logic into its own class

---
+++ src/js/player.js
@@ -0,0 +1,206 @@
+import {
+  JUMP_VELOCITY,
+  MAXIMUM_JUMPS,
+  WALKING_VELOCITY,
+} from '@src/js/constants';
+
+/**
+ * Bind animations to the scene the player lives in and the player itself
+ *
+ * @param {object} scene the scene that the player has been initialized in
+ */
+const setupAnimations = scene => {
+  scene.anims.create({
+    key: 'stand',
+    frames: [{ key: 'player', frame: 0 }],
+  });
+
+  scene.anims.create({
+    key: 'walk',
+    frames: scene.anims.generateFrameNumbers('player', {
+      start: 1,
+      end: 2,
+    }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  scene.anims.create({
+    key: 'airborn',
+    frames: [{ key: 'player', frame: 5 }],
+  });
+
+  scene.anims.create({
+    key: 'jump',
+    frames: [{ key: 'player', frame: 3 }],
+    repeat: -1,
+  });
+
+  scene.anims.create({
+    key: 'double jump',
+    frames: [{ key: 'player', frame: 4 }],
+    repeat: -1,
+  });
+};
+
+/**
+ * The player class
+ *
+ * Handles all player related information:
+ * Physics,
+ * Jumping,
+ * Animations,
+ * Controls
+ *
+ * Before instantiation, ensure that a sprite sheet is loaded for the player.
+ * setupAnimations() may need to be updated if the spritesheet changes and frames are reordered.
+ */
+export default class Player {
+  constructor(scene, x, y) {
+    this.scene = scene;
+
+    setupAnimations(this.scene);
+    this.sprite = this.scene.physics.add
+      .sprite(x, y, 'player')
+      .setBounce(0.15)
+      .setScale(1.5)
+      .setCollideWorldBounds(true);
+  }
+
+  /* Dynamic Player Information */
+  jumpHeight = JUMP_VELOCITY;
+  jumpsLeft = MAXIMUM_JUMPS;
+  jumpLock = false;
+
+  /* Useful getters to phaser properties */
+  getAnims = () => this.sprite.anims;
+  getBody = () => this.sprite.body;
+  getControls = () => this.scene.cursors;
+
+  /* Human readable statuses translated from phaser properties */
+  getStatus = () => ({
+    // Check if the player is touching the ground
+    isGrounded: this.getBody().touching.down,
+  });
+
+  /* Functions for tracking how many times the player has jumped in air */
+  getJumpsLeft = () => this.jumpsLeft;
+  decrementJumpsLeft = () => this.jumpsLeft--;
+  resetJumpsLeft = () => (this.jumpsLeft = MAXIMUM_JUMPS);
+
+  /* Functions for tracking when the player is allowed to jump */
+  enableJumpLock = () => (this.jumpLock = true);
+  disableJumpLock = () => (this.jumpLock = false);
+  getJumpLock = () => this.jumpLock;
+
+  /* Functions for tracking how high the player can jump */
+  setJumpHeight = height => (this.jumpHeight = height);
+  getJumpHeight = () => this.jumpHeight;
+  resetJumpHeight = () => (this.jumpHeight = JUMP_VELOCITY);
+
+  /**
+   * Handles the players ability to jump
+   */
+  handleJumping = () => {
+    const {
+      getControls,
+      getBody,
+      getStatus,
+      getJumpsLeft,
+      setJumpHeight,
+      getJumpHeight,
+      resetJumpHeight,
+      resetJumpsLeft,
+      decrementJumpsLeft,
+      enableJumpLock,
+      disableJumpLock,
+      getJumpLock,
+    } = this;
+    const playerStatus = getStatus();
+    const body = getBody();
+    const cursors = getControls();
+
+    // Don't duplicate jumping velocity while the up arrow is held
+    if (cursors.up.isUp) {
+      disableJumpLock();
+    }
+
+    // Make the second jump in a double jump shorter
+    if (getJumpsLeft() < MAXIMUM_JUMPS) {
+      setJumpHeight(JUMP_VELOCITY + 60);
+    } else {
+      resetJumpHeight();
+    }
+
+    // When the player hits the ground, reset the double jump counter
+    if (playerStatus.isGrounded && !getJumpLock()) {
+      resetJumpsLeft();
+    }
+
+    // Control jumping, allow jumping until the player hits the ground again or there are no more
+    // jumps left
+    if (cursors.up.isDown && !getJumpLock() && getJumpsLeft() > 0) {
+      decrementJumpsLeft();
+      enableJumpLock();
+      body.velocity.y = getJumpHeight();
+    }
+  };
+
+  /**
+   * Handle the animation of the player character while their velocity changes
+   */
+  handleAnimations = () => {
+    const { getBody, getAnims, getJumpsLeft } = this;
+    const body = getBody();
+    const anims = getAnims();
+    const jumpsLeft = getJumpsLeft();
+
+    // While the player is moving horizontally, walk
+    if (body.newVelocity.x != 0) {
+      anims.play('walk', true);
+    } else {
+      anims.play('stand', true);
+    }
+
+    // While the player is falling show that animation
+    if (body.newVelocity.y > 1) {
+      anims.play('airborn');
+    }
+
+    // While the player is moving upwards, show jump animation
+    if (body.newVelocity.y < 0) {
+      if (jumpsLeft === 1) {
+        anims.play('jump');
+      } else {
+        anims.play('double jump');
+      }
+    }
+  };
+
+  /**
+   * Handle the inputs to the player character and the associated animations
+   */
+  handleControls = () => {
+    const { getControls, sprite } = this;
+    const cursors = getControls();
+
+    if (cursors.left.isDown) {
+      sprite.setVelocityX(-WALKING_VELOCITY);
+      sprite.flipX = true;
+    } else if (cursors.right.isDown) {
+      sprite.setVelocityX(WALKING_VELOCITY);
+      sprite.flipX = false;
+    } else {
+      sprite.setVelocityX(0);
+    }
+  };
+
+  /**
+   * Perform all updates to the player on each frame
+   */
+  update = () => {
+    this.handleControls();
+    this.handleJumping();
+    this.handleAnimations();
+  };
+}
--- src/js/scenes/world/create.js
+++ src/js/scenes/world/create.js
@@ -4,9 +4,12 @@ import {
   addImportedSpriteToScene,
   frameDimensions,
 } from '@src/js/util';
+
 import background from '@src/assets/images/background.png';
 import ground from '@src/assets/images/ground.png';
 import hero from '@src/assets/images/hero.png';
+
+import Player from '@src/js/player';
 
 /**
  * Asynchronously load data-uri assets into a scene
@@ -20,7 +23,7 @@ const loadAssets = async function(ctx) {
       await addImportedImageToScene('bg', background, ctx);
       await addImportedImageToScene('ground', ground, ctx);
       await addImportedSpriteToScene(
-        'hero',
+        'player',
         hero,
         frameDimensions(36, 42),
         ctx
@@ -52,56 +55,6 @@ const generatePlatforms = ctx => {
 };
 
 /**
- * Statically generate a player based off of the 'hero' sprite
- * per https://phaser.io/tutorials/making-your-first-phaser-3-game/part5
- *
- * @param {object} ctx desired 'this' to load assets into
- */
-const generatePlayer = ctx => {
-  // Setup player and physics
-  const player = ctx.physics.add
-    .sprite(40, 400, 'hero')
-    .setBounce(0.15)
-    .setScale(1.5)
-    .setCollideWorldBounds(true);
-
-  /* Setup player animations, gonna need tweaking */
-  ctx.anims.create({
-    key: 'stand',
-    frames: [{ key: 'hero', frame: 0 }],
-  });
-
-  ctx.anims.create({
-    key: 'walk',
-    frames: ctx.anims.generateFrameNumbers('hero', {
-      start: 1,
-      end: 2,
-    }),
-    frameRate: 20,
-    repeat: -1,
-  });
-
-  ctx.anims.create({
-    key: 'airborn',
-    frames: [{ key: 'hero', frame: 5 }],
-  });
-
-  ctx.anims.create({
-    key: 'jump',
-    frames: [{ key: 'hero', frame: 3 }],
-    repeat: -1,
-  });
-
-  ctx.anims.create({
-    key: 'double jump',
-    frames: [{ key: 'hero', frame: 4 }],
-    repeat: -1,
-  });
-
-  return player;
-};
-
-/**
  * Create the game and additionally load data-uri assets as base64
  */
 export default async function() {
@@ -116,10 +69,10 @@ export default async function() {
     this.platforms = generatePlatforms(this);
 
     // Setup the player
-    this.player = generatePlayer(this);
+    this.player = new Player(this, 50, 0);
 
     // Add collision between player and platforms
-    this.physics.add.collider(this.player, this.platforms);
+    this.physics.add.collider(this.player.sprite, this.platforms);
 
     // Setup arrow key polling
     this.cursors = this.input.keyboard.createCursorKeys();
